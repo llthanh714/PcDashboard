18562fc1185c5df095f9e03b41048c34407da1cd
llthanh714@live.com <llthanh714@live.com>
2022-07-12 16:30:53 +0700

    Them datepicker

---
+++ assets/js/charts/circles.js
@@ -0,0 +1,14 @@
+Circles.create({
+    id: 'circles-1',
+    radius: 45,
+    value: 68,
+    maxValue: 100,
+    width: 7,
+    text: 68,
+    colors: ['#f1f1f1', '#FF9E27'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    styleWrapper: true,
+    styleText: true
+})
