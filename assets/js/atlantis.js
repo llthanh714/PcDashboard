11567b2c70817a1aa8feb21bdd5000c2f57fac33
llthanh714@live.com <llthanh714@live.com>
2022-07-13 09:01:49 +0700

    Tao ham goi bieu do tron

--- assets/js/atlantis.js
+++ assets/js/atlantis.js
@@ -387,3 +387,29 @@ $('.form-floating-label .form-control').keyup(function(){
 	}
 })
 
+//Datetime Picker
+$(function () {
+
+	var start = moment().subtract(29, 'days');
+	var end = moment();
+
+	function cb(start, end) {
+		$('#reportrange span').html(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
+	}
+
+	$('#reportrange').daterangepicker({
+		startDate: start,
+		endDate: end,
+		ranges: {
+			'Today': [moment(), moment()],
+			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+			'This Month': [moment().startOf('month'), moment().endOf('month')],
+			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+		}
+	}, cb);
+
+	cb(start, end);
+
+});
--- assets/js/charts/circles.js
+++ assets/js/charts/circles.js
@@ -1,14 +1,16 @@
-Circles.create({
-    id: 'circles-1',
-    radius: 45,
-    value: 68,
-    maxValue: 100,
-    width: 7,
-    text: 68,
-    colors: ['#f1f1f1', '#FF9E27'],
-    duration: 400,
-    wrpClass: 'circles-wrp',
-    textClass: 'circles-text',
-    styleWrapper: true,
-    styleText: true
-})
+function createCircles(id, value, color) {
+    Circles.create({
+        id: id,
+        radius: 45,
+        value: value,
+        maxValue: 100,
+        width: 7,
+        text: value,
+        colors: ['#f1f1f1', color],
+        duration: 400,
+        wrpClass: 'circles-wrp',
+        textClass: 'circles-text',
+        styleWrapper: true,
+        styleText: true
+    })
+}
